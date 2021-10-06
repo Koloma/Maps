555624b77281f04ad83a55be264ca7d13293f67a
Alexandr Kolomewskiy <koloma2@mail.ru>
2021-10-06 19:35:42 +0700

    Initial Commit

---
+++ Maps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by Коломенский Александр on 06.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
