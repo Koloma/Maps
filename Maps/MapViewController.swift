d235157ae109545ea24f2bdef4fedf76e51bdcd1
Alexandr Kolomewskiy <koloma2@mail.ru>
2021-10-06 23:25:09 +0700

    add LocationManager

--- Maps/MapViewController.swift
+++ Maps/MapViewController.swift
@@ -14,21 +14,42 @@ class MapViewController: UIViewController {
 
     //Moscov center
     private let coordinateMoscovCenter = CLLocationCoordinate2D(latitude: 55.753215, longitude: 37.622504)
+    private let locationManager = CLLocationManager()
+    private let defaultZoom: Float = 17.0
+
+    private var trackLocation = false
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        confiMap()
+        setupMap()
+        setupLocationManager()
     }
 
     @IBAction func tapAddMarketButton(_ sender: UIBarButtonItem) {
         addMarker(at: coordinateMoscovCenter)
     }
 
-    private func confiMap() {
+    @IBAction func actionTrackLocation(_ sender: UIBarButtonItem) {
+        if trackLocation{
+            locationManager.stopUpdatingLocation()
+            trackLocation = false
+        } else{
+            locationManager.startUpdatingLocation()
+            trackLocation = true
+        }
+    }
+
+    private func setupLocationManager(){
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self;
+    }
+
+    private func setupMap() {
         //Moscov center
         //let coordinate = CLLocationCoordinate2D(latitude: 55.753215, longitude: 37.622504)
-        let camera = GMSCameraPosition.camera(withTarget: coordinateMoscovCenter, zoom: 12)
+        let camera = GMSCameraPosition.camera(withTarget: coordinateMoscovCenter, zoom: defaultZoom)
         mapView.camera = camera
 
         mapView.settings.compassButton = true
@@ -64,8 +85,25 @@ extension MapViewController: GMSMapViewDelegate{
     }
 
     func didTapMyLocationButton(for mapView: GMSMapView) -> Bool {
-        let camera = GMSCameraPosition.camera(withTarget: coordinateMoscovCenter, zoom: 12)
+        let camera = GMSCameraPosition.camera(withTarget: coordinateMoscovCenter, zoom: defaultZoom)
         mapView.animate(to: camera)
         return true
+    }
+}
+
+// MARK: CLLocationManagerDelegate
+extension MapViewController: CLLocationManagerDelegate{
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard
+            let location = locations.last
+        else { return }
+        addMarker(at: location.coordinate)
+        let camera = GMSCameraPosition.camera(withTarget: location.coordinate, zoom: defaultZoom)
+        mapView.camera = camera
     }
 }
