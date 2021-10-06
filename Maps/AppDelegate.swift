ed8c3ff5a427376ea2c7bfa74093b5c5c380da48
Alexandr Kolomewskiy <koloma2@mail.ru>
2021-10-06 20:27:00 +0700

    add GoogleMaps

--- Maps/AppDelegate.swift
+++ Maps/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let aceptGMPkey = GMSServices.provideAPIKey("AIzaSyA97ysHp32tXw3ZZyfdQkJL1ub2kvbDGY8")
+        print(aceptGMPkey)
         return true
     }
 
