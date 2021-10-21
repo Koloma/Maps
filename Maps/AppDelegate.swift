d37adce249b92bbec3151984614d093ad80dc81b
Alexandr Kolomewskiy <koloma2@mail.ru>
2021-10-21 15:23:19 +0700

    turn off autocorrection

--- Maps/AppDelegate.swift
+++ Maps/AppDelegate.swift
@@ -12,8 +12,6 @@ import RealmSwift
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyA97ysHp32tXw3ZZyfdQkJL1ub2kvbDGY8")
